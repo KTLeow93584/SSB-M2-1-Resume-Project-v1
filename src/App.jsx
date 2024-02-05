f3869228eef241b8fd07a4e49710e2130bc400f3
Leow Kean Tat <leowkeantat@gmail.com>
2024-02-05 21:13:26 +0800

    Initial Commit.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Leow Kean Tat (Kaz)</h1>
+      <h2>Software Developer</h2>
+    </>
+  )
+}
+
+export default App
